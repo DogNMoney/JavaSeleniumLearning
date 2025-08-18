073de77615bb9a55bd087210aa214dbcc567c8c3
pawel.pietka <pawel.pietka@synlab.com>
2025-08-18 20:34:13 +0200

    Implemented Mobile View and fixed Screenshots after parallel tests implementation

--- src/main/java/TestConfig.java
+++ src/main/java/TestConfig.java
@@ -1,10 +1,15 @@
 public class TestConfig {
     private String browser;
+    private boolean mobile;
     private boolean fullScreen;
     private boolean headless;
 
     public String getBrowser() {
         return browser;
+    }
+
+    public boolean isMobile(){
+        return mobile;
     }
 
     public boolean isFullScreen() {
